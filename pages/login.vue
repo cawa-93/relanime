b92002a5987b38f3df71203e2fc532bf53b1369e
Alex Kozack <kozackunisoft@gmail.com>
2018-03-12 22:17:21 +0200

    Disable all

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <v-flex xs12 sm8 md4>
-    <v-card class="elevation-12">
+<!--     <v-card class="elevation-12">
       <v-card-title primary-title key="hello-text">
         <p class="headline mb-2">Приветствуем!</p>
         <p>Мы автоматически просканируем список просмотренного вами аниме, чтобы найти то, что вы ещё не видели!</p>
@@ -19,23 +19,23 @@
           <v-text-field placeholder="Введите название" name="search" flat v-model="mainSearch"></v-text-field>
         </form>
       </v-card-text>
-    </v-card>
+    </v-card> -->
   </v-flex>
 </template>
 
 <script>
-  import qs from 'qs'
+  // import qs from 'qs'
   export default {
-  	layout: 'center',
-  	data () {
-  		return {
-    		loginUrl: 'https://shikimori.org/oauth/authorize?' + qs.stringify({
-          client_id: process.env.CLIENT_ID,
-          redirect_uri: process.env.REDIRECT_URI,
-          response_type: 'code'
-        }),
-  			mainSearch: ''
-  		}
-  	}
+  	// layout: 'center',
+  	// data () {
+  	// 	return {
+   //  		loginUrl: 'https://shikimori.org/oauth/authorize?' + qs.stringify({
+   //        client_id: process.env.CLIENT_ID,
+   //        redirect_uri: process.env.REDIRECT_URI,
+   //        response_type: 'code'
+   //      }),
+  	// 		mainSearch: ''
+  	// 	}
+  	// }
   }
 </script>

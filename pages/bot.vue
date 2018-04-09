efd7526f9cb85348153b6afa265f8f46899e9b52
Alex Kozack <kozackunisoft@gmail.com>
2018-04-09 12:51:27 +0300

    Add web-demo to bot.vue

--- pages/bot.vue
+++ pages/bot.vue
@@ -1,45 +1,50 @@
 <template>
   <v-flex
     xs12
-    sm8
-    md4>
-    <v-card class="elevation-12">
-      <v-card-title primary-title>
-        <h1 class="headline mb-2">Чат бот для поиска аниме</h1>
-        <p>Используйте нашего чат-бота, чтобы моментально подобрать любые аниме по вашему вкусу</p>
-      </v-card-title>
-      <v-list>
-        <template v-for="(link, i) in links">
-          <v-list-tile
-            :key="link.title"
+    md10>
+    <v-container class="grid-list-xl">
+      <v-layout
+        row
+        wrap>
+        <v-flex
+          xs12
+          sm6>
+          <h1 class="display-1">Фокси<br><span class="subheading grey--text"> — индивидуальный подбор аниме по вкусу</span></h1>
+          <p>Ты можешь искать аниме по жанру (например, фантастика, повседневность, меха и т.д.) или названию. Я даже могу найти тебе лучшие аниме в своем жанре и показать тебе их целыми списками! Только спроси!</p>
+          <p>Вот, что у меня часто спрашивают:</p>
+          <ul>
+            <li>лучшие аниме</li>
+            <li>школа</li>
+            <li>Тетрадь смерти</li>
+            <li>список аниме в жанре этти</li>
+          </ul>
+
+          <p>И не ругай меня, я только учусь ☺️</p>
+          <v-btn
+            v-for="link in links"
+            :key="link.url"
             :href="link.url"
-            target="_blank">
-            <v-list-tile-action>
-              <img
-                :src="link.icon"
-                :alt="link.title+' icon'"
-                height="40">
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="link.title"/>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-divider
-            :key="`${link.title}-divider`"
-            inset
-            v-if="i+1 < links.length"/>
-        </template>
-      </v-list>
-    </v-card>
-    <p class="text-xs-center mt-5">
-      <nuxt-link to="/">Вернуться на главную</nuxt-link>
-    </p>
-    <p class="text-xs-center mt-5">
-      <a
-        class="caption grey--text"
-        target="_blank"
-        href="https://github.com/cawa-93/dialogflow-anime/blob/master/Privacy%20Policy.txt">Privacy Policy</a>
-    </p>
+            target="_blank"
+            icon
+            fab>
+            <img
+              height="40px"
+              :src="link.icon"
+              :alt="link.title">
+          </v-btn>
+        </v-flex>
+
+        <v-flex
+          xs12
+          sm6
+          class="text-xs-center">
+          <iframe
+            class="elevation-12"
+            height="530"
+            src="https://console.dialogflow.com/api-client/demo/embedded/f0afbab9-ebd7-4520-8122-1354510b0c79"/>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-flex>
 </template>
 
@@ -67,5 +72,9 @@ export default {
     font-size: 2em;
     width: 70px;
     height: 70px;
+  }
+  iframe {
+    border: 0;
+    width: 100%;
   }
 </style>

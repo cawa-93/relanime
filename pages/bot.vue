563345922ac1c099c61a3e1824f03bc7acfab515
Alex Kozack <kozackunisoft@gmail.com>
2018-04-13 00:15:21 +0300

    New bot.vue page

--- pages/bot.vue
+++ pages/bot.vue
@@ -1,15 +1,71 @@
 <template>
   <v-flex
-    xs12
-    md10>
-    <v-container class="grid-list-xl">
+    xs12>
+    <v-container class="pa-0">
       <v-layout
         row
         wrap>
         <v-flex
           xs12
-          sm6>
-          <h1 class="display-1">Фокси<br><span class="subheading grey--text"> — индивидуальный подбор аниме по вкусу</span></h1>
+          md6
+          pa-0
+          mb-3>
+          <div class="iphone-container">
+            <transition-group
+              name="slide-y-reverse-transition"
+              tag="div"
+              class="messages-wrapper">
+              <div
+                class="message-container mt-1"
+                v-for="(mess, i) in messages"
+                :class="{'out':mess.out}"
+                :key="i">
+                <div
+                  class="message">
+                  <v-avatar
+                    size="36"
+                  >
+                    <img
+                      v-if="mess.ava"
+                      src="/alice.png"
+                      alt="avatar">
+                  </v-avatar>
+                  <div
+                    class="message-text pa-2 my-0"
+                    :class="{
+                      'grey lighten-2': mess.out,
+                      'accent white--text': !mess.out,
+                    }"
+                    v-html="mess.text"/>
+                </div>
+              </div>
+
+            </transition-group>
+          </div>
+          <!-- <iframe
+            class="elevation-12"
+            height="530"
+            src="https://console.dialogflow.com/api-client/demo/embedded/f0afbab9-ebd7-4520-8122-1354510b0c79"/>
+        </v-flex> -->
+        </v-flex>
+        <v-flex
+          xs12
+          md6>
+          <h1 class="display-1 text-xs-center text-md-left mb-2">Фокси<br><span class="subheading grey--text">индивидуальный подбор аниме по вкусу</span></h1>
+          <div class="text-xs-center text-sm-left mb-2">
+            <v-btn
+              v-for="link in links"
+              :key="link.url"
+              :href="link.url"
+              target="_blank"
+              icon
+              fab>
+              <img
+                height="40px"
+                :src="link.icon"
+                :alt="link.title">
+            </v-btn>
+          </div>
           <p>Ты можешь искать аниме по жанру (например, фантастика, повседневность, меха и т.д.) или названию. Я даже могу найти тебе лучшие аниме в своем жанре и показать тебе их целыми списками! Только спроси!</p>
           <p>Вот, что у меня часто спрашивают:</p>
           <ul>
@@ -20,31 +76,13 @@
           </ul>
 
           <p>И не ругай меня, я только учусь ☺️</p>
-          <v-btn
-            v-for="link in links"
-            :key="link.url"
-            :href="link.url"
-            target="_blank"
-            icon
-            fab>
-            <img
-              height="40px"
-              :src="link.icon"
-              :alt="link.title">
-          </v-btn>
         </v-flex>
 
-        <v-flex
-          xs12
-          sm6
-          class="text-xs-center">
-          <iframe
-            class="elevation-12"
-            height="530"
-            src="https://console.dialogflow.com/api-client/demo/embedded/f0afbab9-ebd7-4520-8122-1354510b0c79"/>
-        </v-flex>
       </v-layout>
     </v-container>
+    <p class="mt-5 text-xs-center"><nuxt-link
+      to="/"
+      class="grey--text">Главная</nuxt-link></p>
   </v-flex>
 </template>
 
@@ -60,7 +98,33 @@ export default {
       {title: 'Telegram', icon: '/telegramIcon.svg', url: 'https://t.me/animeFoxBot'},
       {title: 'Skype', icon: 'skypeIcon.svg', url: 'https://join.skype.com/bot/a6d9971b-a7ed-476b-980d-e1fc41db0d12'},
 		],
+		messages: [],
 	}),
+	mounted () {
+		const messagesToShow = [
+      {out: 0, ava: 1, interval: 1, text: 'Привет'},
+      {out: 0, ava: 0, interval: 1, text: 'Я — Фокси'},
+      {out: 0, ava: 0, interval: 1, text: 'Какое аниме тебе нравится?'},
+      {out: 1, ava: 0, interval: 2, text: 'Посоветуй лучший триллер или детектив'},
+      {out: 0, ava: 1, interval: 2, text: 'Думаю, <a href="/1535">«Тетрадь смерти»</a> Тебе понравится'},
+      {out: 1, ava: 0, interval: 5, text: 'А что-то более свежее?'},
+      {out: 0, ava: 1, interval: 2, text: 'Хм...'},
+      {out: 0, ava: 0, interval: 1, text: 'Как насчет <a href="/37291">«Рассвет мира»</a>?'},
+      {out: 1, ava: 0, interval: 3, text: 'Я бы хотел какой-то сериал, а не ONA'},
+      {out: 0, ava: 1, interval: 2, text: 'В таком случае, <a href="/35994">«Ангел кровопролития»</a> — TV Сериал'},
+      {out: 1, ava: 0, interval: 5, text: 'А что есть из комедий?'},
+      {out: 0, ava: 1, interval: 3, text: 'Из свежих комедийных сериалов могу посоветовать <a href="/33010">«Фури-кури 2»</a>'},
+      {out: 1, ava: 0, interval: 3, text: 'А что eщё?'},
+      {out: 0, ava: 1, interval: 3, text: '... и <a href="/34134">«Ванпанчмен 2»</a>'},
+      {out: 1, ava: 0, interval: 3, text: 'Это мне подходит. Спасибо.'},
+      {out: 0, ava: 1, interval: 2, text: 'Всегда рада ☺️'},
+      {out: 0, ava: 1, interval: 2, text: 'Пиши мне в месенджере:<br>' + this.links.map(l => `– <a href="${l.url}" target="_blank">${l.title}</a>;`).join('<br>')},
+		]
+		let totalTime = 0
+		messagesToShow.forEach(mess => {
+			setTimeout(() => this.messages.unshift(mess), totalTime += (mess.interval * 1000))
+		})
+	},
 }
 </script>
 
@@ -73,8 +137,60 @@ export default {
     width: 70px;
     height: 70px;
   }
-  iframe {
+  /* iframe {
     border: 0;
     width: 100%;
+  } */
+
+  .iphone-container {
+    background-image: url(/iphonex-frame.png);
+    background-position: top center;
+    padding: 18px 25px 23px;
+    background-size: cover;
+    height: 600px;
+    max-width: 300px;
+    margin: 0 auto;
+    position: relative;
+  }
+  .messages-wrapper {
+    overflow: hidden;
+    display: flex;
+    flex-direction: column-reverse;
+    /* justify-content: flex-end; */
+    height: 100%;
+    border-radius: 24px;
+  }
+  .iphone-container:before {
+    content: "";
+    display: block;
+    height: 20px;
+    background-image: url(/iphonex-head.png);
+    position: absolute;
+    top: 18px;
+    width: 100%;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    left: 0;
+  }
+  .message {
+    display: flex;
+    align-items: center;
+    text-align: left;
+    width: 80%;
+  }
+  .out .message {
+    margin-left: auto;
+    justify-content: flex-end;
+  }
+  .message .avatar {
+    min-width: 36px;
+  }
+
+  .message-text {
+    border-radius: 10px;
+  }
+  .message-text a {
+    color: inherit;
   }
 </style>
